2a94572b814903170379f3d624dc6476fba2c29f
Claudia Pedro dos Santos <pedro.claudia1515@gmail.com>
2022-10-22 19:53:29 +0200

    AuthGuard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './_auth/auth.guard';
 import { ErrorComponent } from './_error/error/error.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule)},//on utile le lazyloading (on evite de chargé le navigateur), ce qui va faire que ce module (public) est chargé uniquement lorsqu'il est demandé
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate:[AuthGuard]},
   { path: '**', component: ErrorComponent },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
